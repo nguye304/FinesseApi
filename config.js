7b790923cabc3808b12fc5bb8ad6a75eb7973c11
Kevin Nguyen <30872809+nguye304@users.noreply.github.com>
2019-06-07 22:55:09 -0700

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-require('dotenv').config();
-
-exports.RDS_HOSTNAME=process.env.hostname||"finessedb.chn5rdataxwt.us-east-1.rds.amazonaws.com";
-exports.RDS_USERNAME=process.env.username||"nguye304";
-exports.RDS_PASSWORD=process.env.password||"kip742Jeg";
-exports.RDS_DATABASE=process.env.database||"finessenailsdb";
-exports.RDS_PORT=process.env.port||"3306";
